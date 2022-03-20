7b544865004792d57ea6f3b2ecee220409d384c4
Dillo Raju <dilloshion@gmail.com>
2022-03-20 11:31:46 -0400

    String problems and solutions

---
+++ easy/strings/reverse_string.rb
@@ -0,0 +1,47 @@
+﻿=begin
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+=end
+
+def reverse_string(str)
+  return [] if str.nil? || str.length < 1
+
+  str_arr = str.each_char.to_a
+
+  return str_arr if str_arr.length == 1
+
+  for i in 0...str_arr.length/2 do
+    j = str_arr.length - i - 1
+    cache_i = str_arr[i]
+    str_arr[i] = str_arr[j]
+    str_arr[j] = cache_i
+  end
+
+  return str_arr
+end
+
+require 'minitest/autorun'
+
+describe 'assertions' do
+  it 'is an empty array if blank' do
+    assert_equal(reverse_string(''), [])
+  end
+
+  it "returns ['a']" do
+    assert_equal(reverse_string('a'), ['a'])
+  end
+
+  it "returns ['d', 'c', 'b', 'a']" do
+    assert_equal(reverse_string('abcd'), ['d', 'c', 'b', 'a'])
+  end
+end
---
+++ easy/strings/valid_anagram.rb
@@ -0,0 +1,63 @@
+﻿=begin
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+=end
+
+def valid_annagram?(str_one, str_two)
+  return false if str_one.nil? || str_two.nil?
+  return false if str_one.length != str_two.length
+
+  ascii_arr = Array.new(256, 0)
+
+  str_one.downcase.each_byte { |b| ascii_arr[b] += 1 }
+  str_two.downcase.each_byte { |b| ascii_arr[b] -= 1 }
+
+  ascii_arr.all? { |i| i == 0 }
+end
+
+require 'minitest/autorun'
+
+describe 'assertions' do
+  it 'is fasle if nil string case 1' do
+    str_one = nil
+    str_two = 'a'
+
+    assert_equal(valid_annagram?(str_one, str_two), false)
+  end
+
+  it 'is fasle if nil string case 2' do
+    str_one = 'foo'
+    str_two = nil
+
+    assert_equal(valid_annagram?(str_one, str_two), false)
+  end
+
+  it 'is false if different lengths' do
+    str_one = 'abc'
+    str_two = 'abcd'
+
+    assert_equal(valid_annagram?(str_one, str_two), false)
+  end
+
+  it 'is true case 1' do
+    assert_equal(valid_annagram?('abc', 'bca'), true)
+  end
+
+  it 'is true case 2' do
+    assert_equal(valid_annagram?('code academy', 'academy code'), true)
+  end
+
+  it 'is true case 3' do
+    assert_equal(valid_annagram?('ANAGRAM', 'gramana'), true)
+  end
+end
