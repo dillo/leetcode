402e0fcbc24d4fb77e99c8f515d97f37db134598
Dillo Raju <dilloshion@gmail.com>
2022-04-27 14:08:48 -0400

    WIP

---
+++ easy/strings/first_unique_char.rb
@@ -0,0 +1,70 @@
+﻿=begin
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+=end
+
+def first_unique_char(str)
+  return 0 if str.length <= 1
+
+  char_count = {}
+  i = 0
+
+  while i < str.length
+    char_count[str[i]] ||= 0
+    char_count[str[i]] += 1
+    i += 1
+  end
+
+  i = 0
+
+  while i < str.length
+    if char_count[str[i]] == 1
+      return i
+    end
+
+    i += 1
+  end
+
+  return -1
+end
+
+require 'minitest/autorun'
+
+describe 'assertions' do
+  it 'should return 0 for a' do
+    assert_equal(first_unique_char('a'), 0)
+  end
+
+  it 'should return 0 for ''' do
+    assert_equal(first_unique_char(''), 0)
+  end
+
+  it 'should return -1 for aabb' do
+    assert_equal(first_unique_char('aabb'), -1)
+  end
+
+  it 'should return 0 for leetcode' do
+    assert_equal(first_unique_char('leetcode'), 0)
+  end
+
+  it 'should return 2 for loveleetcode' do
+    assert_equal(first_unique_char('loveleetcode'), 2)
+  end
+end
--- easy/strings/reverse_integer.rb
+++ easy/strings/reverse_integer.rb
@@ -20,3 +20,39 @@ Constraints:
 
 -231 <= x <= 231 - 1
 =end
+
+def reverse_integer(num)
+  result = 0
+  negative = num.negative?
+  num = num.abs
+
+  while num > 0
+    current = num % 10
+    result = result * 10 + current
+    num /= 10
+  end
+
+  return 0 unless result<2**31
+
+  negative ? -result : result
+end
+
+require 'minitest/autorun'
+
+describe 'assertions' do
+  it 'should return 1 for 1' do
+    assert_equal(reverse_integer(1), 1)
+  end
+
+  it 'should return 963 for 369' do
+    assert_equal(reverse_integer(369), 963)
+  end
+
+  it 'should return -321 for -123' do
+    assert_equal(reverse_integer(-123), -321)
+  end
+
+  it 'should return 0 for 1534236469' do
+    assert_equal(reverse_integer(1534236469), 0)
+  end
+end
